b3d81a46fbae0a760e43220713d6f9f2d5770ae4
Dev Choudhary <devsng999@gmail.com>
2022-08-11 18:52:06 +0530

    my commit

---
+++ README.md
@@ -0,0 +1,41 @@
+# DataCoin
+### Created Using ReactJS,Chart.js,Bootstrap and Hooks using CoinGecko API.
+* Cryptocurrency tracker Application build using Chart.js.
+* User can select any cryptocurrency to view its Current price and price Change in Indian Rupees.
+* Interactive Chart Component which allows user to visualize price change for past 24 hrs,7 days and 1 year.
+* User can add as well as delete any number of cryptocurrency in their dashboard.
+* Includes other miscellaneous information about the selected Cryptocurrency.
+
+
+## Prerequisites
+
+### Install Node JS ###
+Refer to <https://nodejs.org/en/> to install nodejs
+
+### Install create-react-app ###
+Install create-react-app npm package globally. This will help to easily run the project and also build the source files easily. Use the following command to install create-react-app
+
+```bash
+npm install -g create-react-app
+```
+
+
+Click on the link to see the application
+
+## Cloning and Running the Application in local ##
+Clone the project into local
+
+Install all the npm packages. Go into the project folder and type the following command to install all npm packages
+
+```bash
+npm install
+```
+In order to run the application Type the following command
+
+```bash
+npm start
+```
+<img width="1374" alt="Screenshot 2021-08-21 at 10 33 38 PM" src="https://user-images.githubusercontent.com/51152041/130329843-bdfafd3f-01fe-4329-afa4-78ba4da00855.png">
+<img width="1341" alt="Screenshot 2021-08-21 at 10 33 58 PM" src="https://user-images.githubusercontent.com/51152041/130329846-a7c72972-e298-4cbf-b55e-9b2811c9d88a.png">
+<img width="1320" alt="Screenshot 2021-08-21 at 10 34 05 PM" src="https://user-images.githubusercontent.com/51152041/130329848-e0d1925d-9778-485a-b3f4-532663dd33cf.png">
+<img width="1277" alt="Screenshot 2021-08-21 at 10 34 16 PM" src="https://user-images.githubusercontent.com/51152041/130329853-181f5053-60c9-4a1e-8f0f-0e3251976c9d.png">
---
+++ src/pages/CoinDetailPage.jsx
@@ -0,0 +1,79 @@
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import HistoryChart from "../components/HistoryChart";
+import DataCoin from "../components/DataCoin";
+import coinGecko from "../apis/coinGecko";
+
+const CoinDetailPage = () => {
+  const { id } = useParams();
+  const [DataCoin, setDataCoin] = useState({});
+  const [isLoading, setIsLoading] = useState(false);
+
+  const formatData = (data) => {
+    return data.map((el) => {
+      return {
+        t: el[0],
+        y: el[1].toFixed(2),
+      };
+    });
+  };
+
+  useEffect(() => {
+    const fetchData = async () => {
+      setIsLoading(true);
+      const [day, week, year, detail] = await Promise.all([
+        coinGecko.get(`/coins/${id}/market_chart/`, {
+          params: {
+            vs_currency: "usd",
+            days: "1",
+          },
+        }),
+        coinGecko.get(`/coins/${id}/market_chart/`, {
+          params: {
+            vs_currency: "usd",
+            days: "7",
+          },
+        }),
+        coinGecko.get(`/coins/${id}/market_chart/`, {
+          params: {
+            vs_currency: "usd",
+            days: "365",
+          },
+        }),
+        coinGecko.get("/coins/markets/", {
+          params: {
+            vs_currency: "usd",
+            ids: id,
+          },
+        }),
+      ]);
+      console.log(day);
+
+      setDataCoin({
+        day: formatData(day.data.prices),
+        week: formatData(week.data.prices),
+        year: formatData(year.data.prices),
+        detail: detail.data[0],
+      });
+      setIsLoading(false);
+    };
+
+    fetchData();
+  }, []);
+
+  const renderData = () => {
+    if (isLoading) {
+      return <div>Loading....</div>;
+    }
+    return (
+      <div className="coinlist">
+        <HistoryChart data={DataCoin} />
+        <DataCoin data={DataCoin.detail} />
+      </div>
+    );
+  };
+
+  return renderData();
+};
+
+export default CoinDetailPage;
